70cef196e4b503ddcd7f70e51c56afd98fd3c8eb
Xavier-Zhuwanli <sinoxavier@outlook.com>
2017-02-27 22:06:01 +0800

    abcd

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 #include <QApplication>
 #include <QDebug>
 #include "Helper.h"
-#include "DictEngine.h"
-#include <QCryptographicHash>
+#include <QTextBrowser>
+#include "Request.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Helper::instance()->mExecutableDirectory = QCoreApplication::applicationDirPath();
+QTextBrowser b;
+Request r;
+Response re = r.get("https://www.wiktionary.org/");
+qDebug() << re.statusCode();
+b.setHtml(re.content().c_str());
+b.show();
 
     MainWindow w;
     w.setWindowIcon(QIcon(":/static/app_icon_256.ico"));
